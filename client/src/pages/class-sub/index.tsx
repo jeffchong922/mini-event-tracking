5ce6bf288aa894e6199134f226ea755a471f900f
jeffchong <jeffchong922@outlook.com>
2021-08-04 09:52:51 +0800

    feat(client): classSub与fnSub设置分享功能

--- client/src/pages/class-sub/index.tsx
+++ client/src/pages/class-sub/index.tsx
@@ -1,7 +1,16 @@
 import { View, Text } from "@tarojs/components";
 import { Component } from "react";
+import { mainPages } from "../../app.pages";
 
 class ClassSub extends Component {
+
+  onShareAppMessage () {
+    return {
+      title: '这是ClassSub的分享标题',
+      path: mainPages.classIndex
+    }
+  }
+
   render () {
     return (
       <View className='layout'>
--- client/src/pages/fn-sub/index.tsx
+++ client/src/pages/fn-sub/index.tsx
@@ -1,7 +1,17 @@
+import { useShareAppMessage } from '@tarojs/taro'
 import { View, Text } from "@tarojs/components";
 import { VFC } from "react";
+import { mainPages } from '../../app.pages';
 
 const FnSub: VFC = () => {
+
+  useShareAppMessage(() => {
+    return {
+      title: '这是FnSub的分享标题',
+      path: mainPages.classIndex
+    }
+  })
+
   return (
     <View className='layout'>
       <Text>FnSub</Text>
